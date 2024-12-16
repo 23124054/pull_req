f7aa7dc7d271ebe9e9b63910c05d4d9448ce99e5
sri-0324 <subasrisivakumar3@gmail.com>
2024-12-16 12:08:50 +0530

    Update leap.java

--- leap.java
+++ leap.java
@@ -3,10 +3,7 @@ class leap
 {
 public static void main(String args[])
   {
-    Scanner myobj=new Scanner(System.in);
-    System.out.println("enter a year:");
-    int year =myobj.nextInt();
-    System.out.println(" enter a number is: "+year);
+    int year=2004;
     if (year%4==0)
     {
     System.out.println(" It is a leap year");
